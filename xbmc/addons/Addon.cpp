biopendent/xbmc-cmake

add support for vfs add-ons

xbmc/addons/Addon.cpp
@@ -89,6 +89,7 @@ static const TypeMapping types[] =
    {"xbmc.addon.executable",             ADDON_EXECUTABLE,           1043, "DefaultAddonProgram.png" },
    {"xbmc.audioencoder",                 ADDON_AUDIOENCODER,         200,  "DefaultAddonAudioEncoder.png" },
    {"xbmc.audiodecoder",                 ADDON_AUDIODECODER,         201,  "DefaultAddonAudioDecoder.png" },
+   {"xbmc.vfs",                          ADDON_VFS,                  204,  "DefaultAddonVfs.png" },
    {"xbmc.service",                      ADDON_SERVICE,             24018, "DefaultAddonService.png" }};
 
 const CStdString TranslateType(const ADDON::TYPE &type, bool pretty/*=false*/)
@@ -395,6 +396,7 @@ void CAddon::BuildLibName(const cp_extension_t *extension)
       case ADDON_REPOSITORY:
       case ADDON_AUDIODECODER:
       case ADDON_AUDIOENCODER:
+      case ADDON_VFS:
         {
           CStdString temp = CAddonMgr::Get().GetExtValue(extension->configuration, "@library");
           m_strLibName = temp;

xbmc/cores/dvdplayer/DVDInputStreams/DVDInputStreamTV.cpp
@@ -25,7 +25,11 @@
 #include "filesystem/VTPFile.h"
 #include "filesystem/SlingboxFile.h"
 #include "URL.h"
+#include "addons/VFSEntry.h"
+#include "addons/AddonManager.h"
+#include "utils/StringUtils.h"
 
+using namespace ADDON;
 using namespace XFILE;
 
 CDVDInputStreamTV::CDVDInputStreamTV() : CDVDInputStream(DVDSTREAM_TYPE_TV)
@@ -50,26 +54,48 @@ bool CDVDInputStreamTV::Open(const char* strFile, const std::string& content)
 {
   if (!CDVDInputStream::Open(strFile, content)) return false;
 
-  if(strncmp(strFile, "vtp://", 6) == 0)
+  CURL url(strFile);
+  CStdString strProtocol = url.GetProtocol();
+  StringUtils::ToLower(strProtocol);
+  if (!strProtocol.empty())
   {
-    m_pFile       = new CVTPFile();
-    m_pLiveTV     = ((CVTPFile*)m_pFile)->GetLiveTV();
-    m_pRecordable = NULL;
-  }
-  else if (strncmp(strFile, "sling://", 8) == 0)
-  {
-    m_pFile       = new CSlingboxFile();
-    m_pLiveTV     = ((CSlingboxFile*)m_pFile)->GetLiveTV();
-    m_pRecordable = NULL;
-  }
-  else
-  {
-    m_pFile       = new CMythFile();
-    m_pLiveTV     = ((CMythFile*)m_pFile)->GetLiveTV();
-    m_pRecordable = ((CMythFile*)m_pFile)->GetRecordable();
+    VECADDONS addons;
+    CAddonMgr::Get().GetAddons(ADDON_VFS, addons);
+    for (size_t i=0;i<addons.size();++i)
+    {
+      VFSEntryPtr vfs(boost::static_pointer_cast<CVFSEntry>(addons[i]));
+      if (vfs->HasLiveTV() && vfs->GetProtocols().find(strProtocol) != std::string::npos)
+      {
+        CVFSEntryILiveTVWrapper* wrap = new CVFSEntryILiveTVWrapper(CVFSEntryManager::Get().GetAddon(vfs->ID()));
+        m_pFile = wrap;
+        m_pLiveTV = wrap;
+        m_pRecordable = NULL;
+      }
+    }
   }
 
-  CURL url(strFile);
+  if (!m_pFile)
+  {
+    if(strncmp(strFile, "vtp://", 6) == 0)
+    {
+      m_pFile       = new CVTPFile();
+      m_pLiveTV     = ((CVTPFile*)m_pFile)->GetLiveTV();
+      m_pRecordable = NULL;
+    }
+    else if (strncmp(strFile, "sling://", 8) == 0)
+    {
+      m_pFile       = new CSlingboxFile();
+      m_pLiveTV     = ((CSlingboxFile*)m_pFile)->GetLiveTV();
+      m_pRecordable = NULL;
+    }
+    else
+    {
+      m_pFile       = new CMythFile();
+      m_pLiveTV     = ((CMythFile*)m_pFile)->GetLiveTV();
+      m_pRecordable = ((CMythFile*)m_pFile)->GetRecordable();
+    }
+  }
+
   // open file in binary mode
   if (!m_pFile->Open(url))
   {

xbmc/filesystem/FileDirectoryFactory.cpp
@@ -46,6 +46,7 @@
 #include "utils/StringUtils.h"
 #include "addons/AddonManager.h"
 #include "addons/AudioDecoder.h"
+#include "addons/VFSEntry.h"
 
 using namespace ADDON;
 using namespace XFILE;
@@ -81,6 +82,35 @@ IFileDirectory* CFileDirectoryFactory::Create(const CStdString& strPath, CFileIt
       if (result->ContainsFiles(strPath))
         return result;
       delete result;
+      return NULL;
+    }
+  }
+
+  CAddonMgr::Get().GetAddons(ADDON_VFS, codecs);
+  for (size_t i=0;i<codecs.size();++i)
+  {
+    boost::shared_ptr<CVFSEntry> dec(boost::static_pointer_cast<CVFSEntry>(codecs[i]));
+    if (!strExtension.empty() && dec->HasFileDirectories() &&
+        dec->GetExtensions().find(strExtension) != std::string::npos)
+    {
+      CVFSEntryIFileDirectoryWrapper* wrap = new CVFSEntryIFileDirectoryWrapper(CVFSEntryManager::Get().GetAddon(dec->ID()));
+      if (wrap->ContainsFiles(strPath))
+      {
+        if (wrap->m_items.Size() == 1)
+        {
+          // one STORED file - collapse it down
+          *pItem = *wrap->m_items[0];
+        }
+        else
+        { // compressed or more than one file -> create a dir
+          pItem->SetPath(wrap->m_items.GetPath());
+          return wrap;
+        }
+      }
+      else
+        pItem->m_bIsFolder = true;
+
+      delete wrap;
       return NULL;
     }
   }

PhenomG4/kodi-cmake

xbmc/addons/Addon.cpp
@@ -94,6 +94,7 @@ static const TypeMapping types[] =
    {"xbmc.audioencoder",                 ADDON_AUDIOENCODER,         200,  "DefaultAddonAudioEncoder.png" },
    {"kodi.audiodecoder",                 ADDON_AUDIODECODER,         201,  "DefaultAddonAudioDecoder.png" },
    {"xbmc.service",                      ADDON_SERVICE,             24018, "DefaultAddonService.png" },
+   {"kodi.vfs",                          ADDON_VFS,                   204,  "DefaultAddonVfs.png" },
    {"kodi.resource.language",            ADDON_RESOURCE_LANGUAGE,   24026, "DefaultAddonLanguage.png" },
    {"kodi.resource.uisounds",            ADDON_RESOURCE_UISOUNDS,   24006, "DefaultAddonUISounds.png" },
   };
@@ -400,6 +401,7 @@ void CAddon::BuildLibName(const cp_extension_t *extension)
       case ADDON_AUDIOENCODER:
       case ADDON_CONTEXT_ITEM:
       case ADDON_AUDIODECODER:
+      case ADDON_VFS:
         {
           std::string temp = CAddonMgr::Get().GetExtValue(extension->configuration, "@library");
           m_strLibName = temp;

xbmc/cores/dvdplayer/DVDInputStreams/DVDInputStreamTV.cpp
@@ -21,7 +21,11 @@
 #include "DVDInputStreamTV.h"
 #include "filesystem/SlingboxFile.h"
 #include "URL.h"
+#include "addons/VFSEntry.h"
+#include "addons/AddonManager.h"
+#include "utils/StringUtils.h"
 
+using namespace ADDON;
 using namespace XFILE;
 
 CDVDInputStreamTV::CDVDInputStreamTV() : CDVDInputStream(DVDSTREAM_TYPE_TV)
@@ -46,14 +50,42 @@ bool CDVDInputStreamTV::Open(const char* strFile, const std::string& content)
 {
   if (!CDVDInputStream::Open(strFile, content)) return false;
 
-  if (strncmp(strFile, "sling://", 8) == 0)
+  CURL url(strFile);
+  std::string strProtocol = url.GetProtocol();
+  StringUtils::ToLower(strProtocol);
+  if (!strProtocol.empty())
   {
-    m_pFile       = new CSlingboxFile();
-    m_pLiveTV     = ((CSlingboxFile*)m_pFile)->GetLiveTV();
-    m_pRecordable = NULL;
+    VECADDONS addons;
+    CAddonMgr::Get().GetAddons(ADDON_VFS, addons);
+    for (size_t i=0;i<addons.size();++i)
+    {
+      VFSEntryPtr vfs(std::static_pointer_cast<CVFSEntry>(addons[i]));
+      if (vfs->HasLiveTV() && vfs->GetProtocols().find(strProtocol) != std::string::npos)
+      {
+        CVFSEntryILiveTVWrapper* wrap = new CVFSEntryILiveTVWrapper(CVFSEntryManager::Get().GetAddon(vfs->ID()));
+        m_pFile = wrap;
+        m_pLiveTV = wrap;
+        m_pRecordable = NULL;
+      }
+    }
   }
 
-  CURL url(strFile);
+  if (!m_pFile)
+  {
+    if(strncmp(strFile, "vtp://", 6) == 0)
+    {
+      m_pFile       = new CVTPFile();
+      m_pLiveTV     = ((CVTPFile*)m_pFile)->GetLiveTV();
+      m_pRecordable = NULL;
+    }
+    else if (strncmp(strFile, "sling://", 8) == 0)
+    {
+      m_pFile       = new CSlingboxFile();
+      m_pLiveTV     = ((CSlingboxFile*)m_pFile)->GetLiveTV();
+      m_pRecordable = NULL;
+    }
+  }
+
   // open file in binary mode
   if (!m_pFile->Open(url))
   {

xbmc/filesystem/FileDirectoryFactory.cpp
@@ -44,6 +44,7 @@
 #include "URL.h"
 #include "addons/AddonManager.h"
 #include "addons/AudioDecoder.h"
+#include "addons/VFSEntry.h"
 
 using namespace ADDON;
 using namespace XFILE;
@@ -82,7 +83,35 @@ IFileDirectory* CFileDirectoryFactory::Create(const CURL& url, CFileItem* pItem,
   }
 
 #ifdef HAS_FILESYSTEM
-  
+  CAddonMgr::Get().GetAddons(ADDON_VFS, codecs);
+  for (size_t i=0;i<codecs.size();++i)
+  {
+    std::shared_ptr<CVFSEntry> dec(std::static_pointer_cast<CVFSEntry>(codecs[i]));
+    if (!strExtension.empty() && dec->HasFileDirectories() &&
+        dec->GetExtensions().find(strExtension) != std::string::npos)
+    {
+      CVFSEntryIFileDirectoryWrapper* wrap = new CVFSEntryIFileDirectoryWrapper(CVFSEntryManager::Get().GetAddon(dec->ID()));
+      if (wrap->ContainsFiles(url))
+      {
+        if (wrap->m_items.Size() == 1)
+        {
+          // one STORED file - collapse it down
+          *pItem = *wrap->m_items[0];
+        }
+        else
+        { // compressed or more than one file -> create a dir
+          pItem->SetPath(wrap->m_items.GetPath());
+          return wrap;
+        }
+      }
+      else
+        pItem->m_bIsFolder = true;
+
+      delete wrap;
+      return NULL;
+    }
+  }
+
   if (pItem->IsRSS())
     return new CRSSDirectory();
 
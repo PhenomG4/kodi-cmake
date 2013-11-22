487643c3c2044c9a7a98b699830bdd7bf5d30b48
Arne Morten Kvarving <cptspiff@gmail.com>
2013-11-22 19:32:47 +0100

    add support for vfs add-ons

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -2660,6 +2660,7 @@ void CApplication::Stop(int exitCode)
 #ifdef HAS_FILESYSTEM_SFTP
     CSFTPSessionManager::DisconnectAllSessions();
 #endif
+    CVFSEntryManager::Get().DisconnectAll();
 
 #if defined(TARGET_POSIX) && defined(HAS_FILESYSTEM_SMB)
     smb.Deinit();
@@ -4308,6 +4309,7 @@ void CApplication::ProcessSlow()
 #ifdef HAS_FILESYSTEM_SFTP
   CSFTPSessionManager::ClearOutIdleSessions();
 #endif
+  CVFSEntryManager::Get().ClearOutIdle();
 
   g_mediaManager.ProcessEvents();
 
@@ -4944,6 +4946,7 @@ void CApplication::CloseNetworkShares()
 #ifdef HAS_FILESYSTEM_SFTP
   CSFTPSessionManager::DisconnectAllSessions();
 #endif
+  CVFSEntryManager::Get().DisconnectAll();
 }
 
 void CApplication::RegisterActionListener(IActionListener *listener)
